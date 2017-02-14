96b89efbcb0546e728991c457e4f2736093eadf1
iagogomesoliveira <iagogliveira@hotmail.com>
2017-02-14 13:32:39 -0200

    Update recebe_dados.php

--- recebe_dados.php
+++ recebe_dados.php
@@ -1,11 +1,15 @@
-<?php
+	<?php
 	
 			include 'conecta_mysql.inc';
-		
-			//cadastra o produto			
-			if(isset($_POST["cadastrar"]))
+			$op_1 = $_POST["operacao"];
+			
+			//cadastra produto no banco de dados 			
+			if($op_1 == "cadastrar")
 			{
-				$sql = "INSERT INTO produto (nome,codigo,preco) VALUES ('".$_POST["nome"]."','".$_POST["codigo"]."',".$_POST["preco"].")";
+				$nome = $_POST["nome"];
+				$codigo = $_POST["codigo"];
+				$preco = $_POST["preco"];
+				$sql = "INSERT INTO produto (nome,codigo,preco) VALUES ('$nome','$codigo',$preco)";
 				$resultado = mysqli_query($sql);
 				if($conect->query($sql))
 				{
@@ -13,17 +17,33 @@
 				}else{
 					echo "erro";
 				}
-			}
-      //exclui produto
-			elseif(isset($_POST["excluir"]))
+			}	
+			//exclui produto dobanco de dados
+			else if($op_1 == "excluir")
 			{
-				$sql ="DELETE FROM produto WHERE nome = '".$_POST["nome_ex"]."'";
+				$nome = $_POST["nome_ex"];
+				$sql = "DELETE FROM produto WHERE nome = '$nome'";
 				$resultado = mysqli_query($sql);
 				if($conect->query($sql))
-				{
-					echo "produto excluido";
-				}else{
-					echo "ERRO";
-				}					
+					{echo "produto excluido";}
+				else {echo "ERRO";}
 			}
- ?>
+			//visualiza os produtos do banco
+			else if ($op_1 == "visualizar")
+			{
+				$nome = $_POST["nome_find"];
+				$sql = "SELECT * FROM produto";
+				$resultado = mysqli_query($sql);
+				
+				if ($resultado) {
+				    
+				    while($row = mysqli_fetch_assoc($resultado)) {
+				        echo "Name: " . $row["nome"]. " " . $row["codigo"]. " Preco: ".$row["preco"]. "<br>";
+					 }
+				} else {
+				    echo "0 results";
+				}
+			}	
+
+
+?>
